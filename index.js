c8337541a8db47692881224497410e3f188308f7
Dmitro Loshik <dmitroloshik@gmail.com>
2023-05-09 17:49:24 +0300

    change parameters

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// x - число, n - степень
-const pow = (x, n) => {
-  if (n === 0) {
+// num - число, degree - степень
+const pow = (num, degree) => {
+  if (degree === 0) {
     return 1;
   } else {
-    return x * pow(x, n - 1);
+    return num * pow(num, degree - 1);
   }
 };
 console.log(pow(2, 5));
